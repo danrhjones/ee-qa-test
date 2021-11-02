703a54de54ea1520cb1d6dd6599054fbdfe0ccfd
Dan Jones <dan.rh.jones@gmail.com>
2021-11-02 15:33:09 +0000

    add empty step defs for manual steps

---
+++ src/test/java/hotelbookings/steps/ManualSteps.java
@@ -0,0 +1,16 @@
+package hotelbookings.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class ManualSteps {
+
+    @Given("he is not authenticated")
+    public void notAuthenticated() {
+    }
+
+    @Then("he should see an error that says he is unable to delete the booking")
+    public void heShouldSeeAnErrorThatSaysHeIsUnableToDeleteTheBooking() {
+    }
+}
+
